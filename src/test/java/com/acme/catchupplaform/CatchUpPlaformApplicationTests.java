98dcdf7ca382585cea4c3a1d330bba86d56dffa4
Andres (UPC) <103398708+asotito231@users.noreply.github.com>
2024-05-14 07:56:49 -0500

    InitialCommit

---
+++ src/test/java/com/acme/catchupplaform/CatchUpPlaformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.catchupplaform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchUpPlaformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
